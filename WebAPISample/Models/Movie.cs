5f32298bad9a9b9c150cce9f019f62633cfbb110
Simone Connemara <simoneconnemara@outlook.com>
2019-11-04 10:53:30 -0600

    added migration

--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace WebAPISample.Models
 {
     public class Movie
-    {
+    {   [Key]
         public int MovieId { get; set; }
         public string Title { get; set; }
         public string Director { get; set; }
