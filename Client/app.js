bf47e976764bd502b1e23aea41457e1dd552b509
Jake Navarro <jt10navarro@gmail.com>
2019-11-05 15:37:55 -0600

    started POST method

--- Client/app.js
+++ Client/app.js
@@ -13,4 +13,19 @@ $('input').on('click', function () {
 });
 });
 
-// ("button").click
+$(document).ready(function(){
+    $("#submit").click(function(){
+    var title = $('#Title').val();
+    var director = $('#Director').val();
+    var genre = $('#Genre').val();
+    $.ajax({
+        type: 'POST',
+        url: 'https://localhost:44352/api/movie',
+        dataType: 'json',
+        data: json.stringify([title, director, genre]),
+        success: function(movie) { },
+        error: function(xhr, textStatus, errorthrown) { alert('error'); },
+        console.log(movie);
+        });
+    });
+});
