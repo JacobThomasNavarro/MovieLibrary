0f5a9bd52e3e7b0c8c82c65d50c9273dd3c271aa
Jake Navarro <jt10navarro@gmail.com>
2019-11-06 09:33:10 -0600

    added post method from starter code and tested

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,31 @@
+(function($){
+    function processForm( e ){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value,
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-form').submit( processForm );
+})(jQuery);
+
 $(document).ready(function(){
 $('input').on('click', function () {
     var $movies = $('#movies');
@@ -6,26 +34,10 @@ $('input').on('click', function () {
         url: 'https://localhost:44352/api/movie',
         success: function(movies){
            $.each(movies, function(i, movie){
-                $movies.append('<li>Title: '+ movie.Title +' Director: '+ movie.Director +' Genre: '+ movie.Genre + '</li>');
+                $movies.html('');
+                $movies.append('<li>Title: '+ movie.Title +' Director: '+ movie.Director +' Genre: '+ movie.Genre + '</li>'); 
             });
         }
     });
 });
-});
-
-$(document).ready(function(){
-    $("#submit").click(function(){
-    var title = $('#Title').val();
-    var director = $('#Director').val();
-    var genre = $('#Genre').val();
-    $.ajax({
-        type: 'POST',
-        url: 'https://localhost:44352/api/movie',
-        dataType: 'json',
-        data: json.stringify([title, director, genre]),
-        success: function(movie) { },
-        error: function(xhr, textStatus, errorthrown) { alert('error'); },
-        console.log(movie);
-        });
-    });
-});
+})
